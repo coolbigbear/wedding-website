2788e353690bc1b3ad765c62a1774277b501067c
Michal Czuper <mrczuper@gmail.com>
2025-03-10 20:01:02 +0000

    Update date

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = ({ t }) => {
 			/>
 			<div className="relative z-10 px-4 max-w-4xl mx-auto">
 				<h1 className="text-5xl md:text-6xl font-bold text-rose-800 mb-4 animate-fade-in-down">{t.title}</h1>
-				<p className="text-xl md:text-2xl text-rose-700 mb-8">someDate</p>
+				<p className="text-xl md:text-2xl text-rose-700 mb-8">{t.date}</p>
 				<div className="bg-white bg-opacity-80 p-6 rounded-lg shadow-lg">
 					<p className="text-xl text-gray-800 mb-4">{t.hero.invitation}</p>
 					<p className="text-lg text-gray-700">{t.hero.details}</p>
