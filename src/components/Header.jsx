fa727ee8c97a486f935d18e682748830d8b8932f
Michal Czuper <mrczuper@gmail.com>
2025-03-16 19:02:19 +0000

    Fix my name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export function Header({ setLanguage }) {
 		<header className="flex sticky top-0 z-50 w-full items-center bg-transparent">
 			<div className="flex h-fit w-full justify-between gap-2 px-4">
 				<div className="bg-white p-2 rounded-3xl m-2">
-					<p className="text-5xl md:text-4xl font-[ephesis] p-2 text-black dark:text-white">Sonali & Michal</p>
+					<p className="text-5xl md:text-4xl font-[ephesis] p-2 text-black dark:text-white">Sonali & Michał</p>
 				</div>
 				<div className="hidden md:flex">
 					<LanguageSelector setLanguage={setLanguage} />
