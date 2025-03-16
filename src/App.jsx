11949ae6cf2c5d79430837a3ce670bbd4631d796
Michal Czuper <mrczuper@gmail.com>
2025-03-16 23:32:32 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ const App = () => {
 				<Routes>
 					<Route path="/" element={<Layout />}>
 						<Route index element={<Home />} />
-						<Route path="about" element={<About />} />
-						{/* <Route path="contact" element={<Contact />} />
+						{/*<Route path="about" element={<About />} />
+						 <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} /> */}
 					</Route>
 				</Routes>
