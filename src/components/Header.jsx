540767170ca1e0564231de99671d6f499cd72425
Michal Czuper <mrczuper@gmail.com>
2025-03-16 19:26:13 +0000

    Use home button instead of names

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,19 @@
 'use client';
+import { Home } from 'lucide-react';
 import LanguageSelector from './LanguageSelector';
 import { MobileMenu } from './MobileMenu';
 export function Header({ setLanguage }) {
 	return (
 		<header className="flex sticky top-0 z-50 w-full items-center bg-transparent">
 			<div className="flex h-fit w-full justify-between gap-2 px-4">
-				<div className="m-2">
-					<p className="text-4xl md:text-4xl font-[ephesis] p-2 text-black dark:text-white bg-white rounded-3xl">
-						Sonali & Michał
-					</p>
+				<div className="flex m-2">
+					<button>
+						<Home/>
+					</button>
 				</div>
-				<div className="hidden md:flex">
-					<LanguageSelector setLanguage={setLanguage} />
-				</div>
+				<LanguageSelector setLanguage={setLanguage} />
 				<div className="flex md:hidden">
 					<MobileMenu>
-						<LanguageSelector setLanguage={setLanguage} />
 						<p href="/about" className="px-2 py-1">
 							About
 						</p>
