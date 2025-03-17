3db8fa52a0749df2474bc9f66b399691f0f51acd
Michal Czuper <mrczuper@gmail.com>
2025-03-17 18:46:35 +0000

    Scroll to top on router change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes, useLocation } from 'react-router-dom';
 import Layout from './components/pages/Layout';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 
-import { createContext, useState } from 'react';
+import { createContext, useLayoutEffect, useState } from 'react';
 import { translations } from './translations';
 import Attire from './components/pages/Attire';
 
 export const LanguageContext = createContext('english');
+
+const Wrapper = ({ children }) => {
+	const location = useLocation();
+	useLayoutEffect(() => {
+		document.documentElement.scrollTo(0, 0);
+	}, [location.pathname]);
+	return children;
+};
 
 const App = () => {
 	const [language, setLanguage] = useState('english');
@@ -15,15 +23,17 @@ const App = () => {
 	return (
 		<LanguageContext.Provider value={{ language: t, setLanguage: setLanguage }}>
 			<HashRouter basename={import.meta.env.BASE_URL}>
-				<Routes>
-					<Route path="/" element={<Layout />}>
-						<Route index element={<Home />} />
-						<Route path="about" element={<About />} />
-						<Route path="attire" element={<Attire />} />
-						{/* <Route path="contact" element={<Contact />} />
+				<Wrapper>
+					<Routes>
+						<Route path="/" element={<Layout />}>
+							<Route index element={<Home />} />
+							<Route path="about" element={<About />} />
+							<Route path="attire" element={<Attire />} />
+							{/* <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} /> */}
-					</Route>
-				</Routes>
+						</Route>
+					</Routes>
+				</Wrapper>
 			</HashRouter>
 		</LanguageContext.Provider>
 	);
