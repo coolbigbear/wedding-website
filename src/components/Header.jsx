d1c072f4f171dcb5067a63c4075133eb4b9de84f
Michal Czuper <mrczuper@gmail.com>
2025-03-20 22:12:21 +0000

    Px fix for desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 export function Header({ setLanguage }) {
 	return (
 		<header className="flex sticky top-0 z-50 w-full items-center bg-transparent">
-			<div className="flex h-fit w-full justify-around md:justify-between md:px-4 md:px-8 md:pt-4">
+			<div className="flex h-fit w-full justify-around md:justify-between md:px-10 md:pt-4">
 				<div className="flex m-4">
 					<Link to="/">
 						<Button variant="ghost" size="icon" className="text-black w-11 h-11 md:w-16 md:h-16 shadow-md">
