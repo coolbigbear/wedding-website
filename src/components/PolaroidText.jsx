7d0ddd9f1a5ab3cb41f2c7fe8f9b9b694c56834c
Michal Czuper <mrczuper@gmail.com>
2025-03-24 14:21:50 +0000

    fade in for about us

--- src/components/PolaroidText.jsx
+++ src/components/PolaroidText.jsx
@@ -1,13 +1,22 @@
-import Polaroid from "./Polaroid";
+import { useRef } from 'react';
+import Polaroid from './Polaroid';
+import { useIsVisible } from './hooks/useIsVisible';
 
-export default function PolaroidText({ text, imageUrl, left=true }) {
-	
-	text = { __html: text};
+export default function PolaroidText({ text, imageUrl, left = true }) {
+	text = { __html: text };
+
+	const ref = useRef();
+	const isVisible = useIsVisible(ref);
 
 	return (
-		<div className={`flex ${left ? "md:flex-row" : "md:flex-row-reverse" } flex-col items-center mx-10 md:mx-[20vw]`}>
+		<div
+			ref={ref}
+			className={`flex flex-col items-center mx-10 md:mx-[20vw]transition-opacity ease-in duration-700
+			${left ? 'md:flex-row' : 'md:flex-row-reverse'}
+			${isVisible ? 'opacity-100' : 'opacity-0'} 
+		`}>
 			<div className="flex z-20 max-w-[70vw] md:text-xl items-center my-5">
-				<div dangerouslySetInnerHTML={text}/>
+				<div dangerouslySetInnerHTML={text} />
 			</div>
 			<Polaroid imageUrl={imageUrl} left={left} />
 		</div>
--- src/components/pages/Events.jsx
+++ src/components/pages/Events.jsx
@@ -4,7 +4,7 @@ import { DescriptionCard } from '../DescriptionCard';
 const Events = ({ language, bgcolor }) => {
 	const markup = { __html: language.description };
 	return (
-		<div className="flex flex-col bg-white font-serif bg-">
+		<div className={`flex flex-col bg-white font-serif`}>
 			<Title title={language.title} subtitle={language.subtitle} bgcolor={bgcolor} />
 			<h1 className="text-2xl md:mx-40 mx-4 text-center text-black my-8" dangerouslySetInnerHTML={markup}></h1>
 			<div className="grid col-auto">
