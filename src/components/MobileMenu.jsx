267ad71d07b64817776726458e586d8dc471d712
Michal Czuper <mrczuper@gmail.com>
2025-03-24 11:24:27 +0000

    Fix side menu when scrolled

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -13,14 +13,14 @@ export function MobileMenu({ children }) {
 		<Sheet open={open} onOpenChange={setOpen}>
 			<SheetTrigger asChild>
 				<div className="flex items-center">
-					<Button variant="ghost" size="icon" className="xl:hidden w-11 h-11 md:w-16 md:h-16">
+					<Button variant="ghost" size="icon" className="xl:hidden w-11 h-11 md:w-16 md:h-16 shadow-md">
 						<Menu className="h-5 w-5" />
 						<span className="sr-only">Toggle menu</span>
 					</Button>
 				</div>
 			</SheetTrigger>
-			<SheetContent side="right" className="absolute w-[240px] sm:w-[300px] h-screen">
-				<div className="flex h-fit flex-col justify-between">
+			<SheetContent side="right" className="w-[240px] sm:w-[300px]">
+				<div className="flex flex-col justify-between">
 					<div className="mt-8 flex flex-col gap-4">
 						{children.map((child, index) => (
 							<SheetTrigger asChild key={index}>
