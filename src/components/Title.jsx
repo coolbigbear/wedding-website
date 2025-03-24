74209eef5df7b6c3182e05b6b91d5fa589b821a4
Michal Czuper <mrczuper@gmail.com>
2025-03-24 12:49:13 +0000

    Change title to w-full

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 export function Title({ title, subtitle, bgcolor }) {
 	return (
 		<div className={`h-1/2 w-full flex text-center`}>
-			<div id="banner" className={`absolute inset-0 h-[10rem] ${bgcolor} w-screen`}></div>
+			<div id="banner" className={`absolute inset-0 h-[10rem] ${bgcolor} w-full`}></div>
 			<div className={`z-30 flex flex-col ${bgcolor} w-screen py-4 px-2 `}>
 				<h1 className="text-5xl md:text-7xl text-white animate-fade-in-down font-[ephesis] outline-black">{title}</h1>
 				<div className="bg-white bg-opacity-0 rounded-lg">
