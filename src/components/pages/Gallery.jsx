84765878178ce29bcb58cfec34ef699071219b50
Michal Czuper <mrczuper@gmail.com>
2026-05-07 21:17:13 +0100

    Remove upload button

--- src/components/pages/Gallery.jsx
+++ src/components/pages/Gallery.jsx
@@ -9,16 +9,6 @@ const Gallery = ({ language, bgcolor }) => {
 			<Title title={language.title} subtitle={language.subtitle} bgcolor={bgcolor} />
 			<h1 className="text-2xl md:mx-40 mx-4 text-center text-black my-8" dangerouslySetInnerHTML={markup}></h1>
 			<div className="grid col-auto">
-				<div className='mx-auto mb-5'>
-					<ImmichButton
-						buttonText={language.uploadText}
-						bgColor={'bg-black'}
-						textColor={'white'}
-						hoverBgColor={'bg-white'}
-						hoverTextColor={'black'}
-						url={'https://immich.coolbigbear.com/share/Syl2hg3evnhW7PwvUcIoYQMOnHHE6Kughza8kIfh_Jvnu4BEEQrgzfdddkw4s2u5dHA'}
-					/>
-				</div>
 				<div className="md:grid md:grid-cols-3 md:gap-8 md:mx-8">
 					{language.events.map((eventObj, index) => (
 						<GalleryCard
